3295cb4c349ae4e6df6534d2e0d79fcbdbc4ece2
yeseul <khys0079@gmail.com>
2022-03-01 23:17:09 +0900

    feat:client model-skeleton & keypoints 시각화 #27

---
+++ ai_service/workout/src/constants.js
@@ -0,0 +1,11 @@
+export const COCO_KEYPOINTS = [
+    'nose', 'left_eye', 'right_eye', 'left_ear', 'right_ear', 'left_shoulder',
+    'right_shoulder', 'left_elbow', 'right_elbow', 'left_wrist', 'right_wrist',
+    'left_hip', 'right_hip', 'left_knee', 'right_knee', 'left_ankle',
+    'right_ankle'
+  ];
+
+export const COCO_CONNECTED_KEYPOINTS_PAIRS = [
+    [0, 1], [0, 2], [1, 3], [2, 4], [5, 6], [5, 7], [5, 11], [6, 8], [6, 12],
+    [7, 9], [8, 10], [11, 12], [11, 13], [12, 14], [13, 15], [14, 16]
+  ];
---
+++ ai_service/workout/src/utilities.js
@@ -0,0 +1,60 @@
+import * as constants from './constants';
+
+const color = "rgb(255, 114, 114)";
+const lineWidth = 4;
+
+function toTuple({ y, x }) {
+  return [y, x];
+}
+
+export function drawPoint(ctx, y, x, r, color) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+
+export function drawSegment([ay, ax], [by, bx], color, scale, ctx) {
+  ctx.beginPath();
+  ctx.moveTo(ax * scale, ay * scale);
+  ctx.lineTo(bx * scale, by * scale);
+  ctx.lineWidth = lineWidth;
+  ctx.strokeStyle = color;
+  ctx.stroke();
+}
+
+export function getAdjacentPairs() {
+      return constants.COCO_CONNECTED_KEYPOINTS_PAIRS;
+  
+}
+
+export function drawSkeleton(keypoints, minConfidence, ctx, scale = 1) {
+  const adjacentKeyPoints=getAdjacentPairs()
+
+
+  adjacentKeyPoints.forEach((line) => {
+    if (keypoints[adjacentKeyPoints.indexOf(line)].score > minConfidence){
+      drawSegment(
+        toTuple(keypoints[line[0]]),
+        toTuple(keypoints[line[1]]),
+        color,
+        scale,
+        ctx
+      );
+    }
+  });
+}
+
+
+
+export function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
+  for (let i = 0; i < keypoints.length; i++) {
+    const keypoint = keypoints[i];
+
+    if (keypoint.score < minConfidence) {
+      continue;
+    }
+
+    drawPoint(ctx, keypoint.y *scale, keypoint.x * scale, 3, color);
+  }}
