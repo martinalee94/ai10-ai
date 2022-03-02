57ae2bc87b961b8c865952d899faf0d208bbd1f1
yeseul <khys0079@gmail.com>
2022-03-03 03:43:52 +0900

    feat:client model-변환된 classifer 적용 #27

--- ai_service/workout/src/App.js
+++ ai_service/workout/src/App.js
@@ -3,25 +3,28 @@ import "./App.css";
 import Webcam from "react-webcam";
 import { drawKeypoints, drawSkeleton } from "./utilities";
 
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-backend-webgl';
+import {loadGraphModel} from '@tensorflow/tfjs-converter';
 import * as poseDetection from '@tensorflow-models/pose-detection';
 
-//import '@tensorflow/tfjs-backend-wasm';
-import '@tensorflow/tfjs-backend-webgl';
+ tf.setBackend("cpu")
+const url='https://raw.githubusercontent.com/yeseulKIM00/test/main/graph/model.json'
+const detectorConfig = {modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER};//SINGLEPOSE_LIGHTNING
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
-const detectorConfig = {modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER};//SINGLEPOSE_LIGHTNING
   const runMovenet = async () => {
     const detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
+    const dnn76= await loadGraphModel(url)
 
     setInterval(() => {
-      detect(detector);
+      detect(detector,dnn76);
     }, 100);
   };
-  
-  const detect = async (detector) => {
+  const detect = async (detector,dnn76) => {
     if (
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null &&
@@ -35,11 +38,38 @@ const detectorConfig = {modelType: poseDetection.movenet.modelType.SINGLEPOSE_TH
       webcamRef.current.video.height = videoHeight;
 
       const pose = await detector.estimatePoses(video);
-
-      console.log(pose);
       drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
+
+      poseClassify(dnn76,pose);
     }
   };
+
+  const poseClassify=(dnn76,pose)=>{
+    if(pose){
+      let inputs=[];
+      for (let i=0 ; i <pose[0].keypoints.length;i++){
+        let x=pose[0]["keypoints"][i].x;
+        let y=pose[0]["keypoints"][i].y;
+        let score=pose[0]["keypoints"][i].score;
+        inputs.push(x);
+        inputs.push(y);
+        inputs.push(score);
+      } 
+      const input_tensor= tf.tensor1d(inputs);
+      const pred= dnn76.predict(input_tensor.reshape([1,51]))
+      console.log("prediction:",pred.dataSync())
+
+      const index =pred.argMax(1).dataSync()[0] 
+      const predict = pred.dataSync(); 
+      const value = predict[index];
+    
+      console.log(index)
+      console.log( value)
+    }
+    else{
+      setTimeout(poseClassify,100);
+    }
+  }
 
   const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
     const ctx = canvas.current.getContext("2d");
@@ -51,6 +81,7 @@ const detectorConfig = {modelType: poseDetection.movenet.modelType.SINGLEPOSE_TH
   };
 
   runMovenet();
+
   return (
     <div className="App">
       <header className="App-header">
